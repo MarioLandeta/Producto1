3082ce6f775666a4487645cef853b6d6c764bc73
mlandetUTC <mario.landeta8@utc.edu.ec>
2016-10-21 22:44:00 -0500

    Login inicial

---
+++ src/crack_obj/Crack_obj.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crack_obj;
+
+/**
+ *
+ * @author MarioJavier
+ */
+public class Crack_obj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
